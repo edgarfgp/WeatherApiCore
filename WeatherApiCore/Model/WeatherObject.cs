287c4d893e376aa0b6de7248c7db6a701d367fbc
Edgar Fabian Gonzalez Peña <edgargonzalez.developer@gmail.com>
2018-01-22 23:19:38 +0100

    WeahterObject  has been updated

--- WeatherApiCore/Model/WeatherObject.cs
+++ WeatherApiCore/Model/WeatherObject.cs
@@ -7,13 +7,115 @@ namespace WeatherApiCore.Model
 {
     public class WeatherObject
     {
+
+        public Coord Coord { get; set; }
+
+
+        public Weather[] Weather { get; set; }
+
+
+        public string Base { get; set; }
+
+
+        public Main Main { get; set; }
+
+
+        public long Visibility { get; set; }
+
+
+        public Wind Wind { get; set; }
+
+
+        public Clouds Clouds { get; set; }
+
+
+        public long Dt { get; set; }
+
+
+        public Sys Sys { get; set; }
+
+
+        public long Id { get; set; }
+
+
+        public string Name { get; set; }
+
+
+        public long Cod { get; set; }
+    }
+
+    public class Clouds
+    {
+
+        public long All { get; set; }
+    }
+
+    public class Coord
+    {
+
+        public double Lon { get; set; }
+
+
+        public double Lat { get; set; }
+    }
+
+    public class Main
+    {
+
+        public double Temp { get; set; }
+
+
+        public long Pressure { get; set; }
+
+
+        public long Humidity { get; set; }
+
+
+        public long TempMin { get; set; }
+
+
+        public long TempMax { get; set; }
+    }
+
+    public class Sys
+    {
+
+        public long Type { get; set; }
+
+
+        public long Id { get; set; }
+
+
+        public double Message { get; set; }
+
+
         public string Country { get; set; }
-        public string City { get; set; }
-        public DateTime LocalTime { get; set; }
-        public string Idiom { get; set; }
-        public long TemperatureMax { get; set; }
-        public long TemperatureMin { get; set; }
 
 
+        public long Sunrise { get; set; }
+
+
+        public long Sunset { get; set; }
+    }
+
+    public class Weather
+    {
+
+        public long Id { get; set; }
+
+
+        public string Main { get; set; }
+
+
+        public string Description { get; set; }
+
+
+        public string Icon { get; set; }
+    }
+
+    public class Wind
+    {
+
+        public double Speed { get; set; }
     }
 }
