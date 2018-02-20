b74b029f6e265f966f9ddec855277850e094b278
Edgar Fabian Gonzalez Peña <edgargonzalez.developer@gmail.com>
2018-02-20 17:18:10 +0100

    Set maxPageSize 20

--- WeatherApiCore/Helpers/CitiesResourcesParameters.cs
+++ WeatherApiCore/Helpers/CitiesResourcesParameters.cs
@@ -7,7 +7,7 @@ namespace WeatherApiCore.Helpers
 {
     public class CitiesResourcesParameters
     {
-        const int maxPageSize = 10;
+        const int maxPageSize = 20;
         public int PageNumber { get; set; } = 1;
 
         private int _pageSize = 5;
